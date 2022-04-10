af094dca10f8f842161dceed2a33a51173fd5c68
Zhiming <i-jovial@hotmail.com>
2022-04-10 14:57:16 +0800

    customize profile photo with rounded and flexible borders

--- pages/index.js
+++ pages/index.js
@@ -14,24 +14,27 @@ export default function Home() {
       {/*Header*/}
       <Header />
 
-      <div className='flex container mx-auto w-64 h-64 items-center m-20'>
-        <Image 
-          className=''
-          src="/max-01.jpg"
-          width={853}
-          height={1280}
-          alt="Picture of Author"
-        />
+      <div className='flex flex-wrap justify-center'>
+        <div className='w-6/12 sm:w-4/12 px-4'>
+          <Image 
+            className='shadow-lg rounded-full max-w-full h-auto 
+            align-middle border-none'
+            src="/max-01.jpg"
+            width={853}
+            height={1280}
+            alt="Picture of Author"
+          />
+        </div>
       </div>
 
-      <div className='flex text-center text-6xl justify-center p-6'>
+      <div className='flex text-center justify-center text-6xl p-6'>
         <p className=''>
           Zhiming Lim
         </p>
       </div>
       
 
-      <div className='font-mono flex text-center justify-evenly'>
+      <div className='font-mono flex text-center justify-evenly mb-20'>
         <p className='hover:animate-pulse'>
           Software Engineer
         </p>
